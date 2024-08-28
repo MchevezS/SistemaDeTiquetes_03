3a9adfb230c9f64e30ba37916395834ccd5e31fb
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-08-28 10:38:02 -0600

    cambios en consultas y en tabla consultas

--- src/js/ConsultasLocal.js
+++ src/js/ConsultasLocal.js
@@ -1,23 +1,30 @@
-const NombreE = document.getElementById("NombreE");
-const Consulta = document.getElementById("consulta");
-const Fecha = document.getElementById("fecha");
+const nombreE = document.getElementById("NombreE");
+const consulta = document.getElementById("consulta");
+const fecha = document.getElementById("fecha");
 const BtnConsulta = document.getElementById("btnEnviar");
+const hora = document.getElementById('hora');
+const tipoDeConsultas = document.getElementById('tipoDeConsultas');
+
 let datosLocalStorage = JSON.parse(localStorage.getItem("consultas")) || [];
 function GuardarConsulta() {
     let Datosconsulta = {
-        nombre: NombreE.value,
-        consulta: Consulta.value,
-        fecha: Fecha.value,
+        nombre: nombreE.value,
+        consulta: consulta.value,
+        fecha: fecha.value,
+        hora:hora.value,
+       tipo:tipoDeConsultas.value,
     };
     datosLocalStorage.push(Datosconsulta);
     localStorage.setItem("consultas", JSON.stringify(datosLocalStorage));
-    NombreE.value = "";
-    Consulta.value = "";
-    Fecha.value = "";
+    nombreE.value = "";
+    consulta.value = "";
+    fecha.value = "";
+    hora.value = "";
+    tipoDeConsultas.value ="";
 }
 BtnConsulta.addEventListener("click", function(e) {
     e.preventDefault();
-    if (NombreE.value === "" || Consulta.value === "" || Fecha.value === "" ) {
+    if (nombreE.value === "" || consulta.value === "" || fecha.value === "" || hora.value === "" ) {
         alert("Por favor llenar todos los campos");
     } else{
         alert("consulta enviada exitosamente ")
